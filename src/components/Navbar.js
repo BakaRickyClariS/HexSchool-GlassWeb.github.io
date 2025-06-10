341b6aa2253e543210cee19b2c5bd1b80feb5313
BakaRickyClariS <ricky860526@gmail.com>
2025-06-10 15:49:25 +0800

    home page 完成(含RWD)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,19 +4,29 @@ class Navbar extends HTMLElement {
     this.innerHTML = `
     <header class="bg-[#AA0601]">
       <nav
-        class="flex flex-row justify-between items-center max-w-screen-xl mx-auto p-5 w-screen"
+        class="flex flex-col md:flex-row md:justify-between md:items-center max-w-screen-xl mx-auto py-5 md:px-10 w-full"
       >
-        <div class="flex">
+        <div class="flex pb-5 pl-5 md:pl-0 md:pb-0">
           <a href="/">
-            <img class="w-16" src="/public/images/logo-white.png" alt="logo" />
+            <img class="h-[48.57px]" src="images/logo-white.png" alt="logo" />
           </a>
         </div>
-        <div class="flex">
-          <ul class="flex flex-row space-x-15 text-white">
-            <li><a href="/course">系列課程</a></li>
-            <li><a href="/location">門市據點</a></li>
-            <li><a href="/blog">部落格</a></li>
-            <li><a href="/problem">常見問題</a></li>
+        <div class="flex w-full md:w-3/5 xl:w-1/2">
+          <ul
+            class="grid grid-cols-2 md:grid-cols-4 space-x-15 text-center text-white text-lg w-full"
+          >
+            <li class="border-2 border-white py-5 w-full md:border-none">
+              <a href="/shop">系列鏡框</a>
+            </li>
+            <li class="border-2 border-white py-5 w-full md:border-none">
+              <a href="/location">門市據點</a>
+            </li>
+            <li class="border-2 border-white py-5 w-full md:border-none">
+              <a href="/blog">部落格</a>
+            </li>
+            <li class="border-2 border-white py-5 w-full md:border-none">
+              <a href="/problem">常見問題</a>
+            </li>
           </ul>
         </div>
       </nav>
